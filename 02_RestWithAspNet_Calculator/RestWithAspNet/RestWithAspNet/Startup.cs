3a78b68f6ae8701323189e9cbf6646b5f93d2934
jhonatacamargol <jhonatapires32@hotmail.com>
2023-01-03 17:55:04 -0300

    novo

--- 02_RestWithAspNet_Calculator/RestWithAspNet/RestWithAspNet/Startup.cs
+++ 02_RestWithAspNet_Calculator/RestWithAspNet/RestWithAspNet/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RestWithAspNet.Model.Context;
 using RestWithAspNet.Services;
 using RestWithAspNet.Services.Implementations;
 using System;
@@ -29,6 +31,9 @@ namespace RestWithAspNet
         {
 
             services.AddControllers();
+
+            var connection = Configuration["MySQLConection : MySQLConectionString"];
+            services.AddDbContext<MySqlContext>(optionsAction=> optionsAction.UseMySql(connection));
 
             //Dependency Injection
             services.AddScoped<IPersonService, PersonServiceImplementation>();
