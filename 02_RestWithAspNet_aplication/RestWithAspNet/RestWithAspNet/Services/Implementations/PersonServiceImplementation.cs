cc7fa5418cd9e1286cf4f91970804d69c1916c38
jhonatacamargol <jhonatapires32@hotmail.com>
2023-01-07 17:04:29 -0300

    api novo

--- 02_RestWithAspNet_aplication/RestWithAspNet/RestWithAspNet/Services/Implementations/PersonServiceImplementation.cs
+++ 02_RestWithAspNet_aplication/RestWithAspNet/RestWithAspNet/Services/Implementations/PersonServiceImplementation.cs
@@ -1,5 +1,7 @@
-﻿using RestWithAspNet.Model;
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using RestWithAspNet.Model;
 using RestWithAspNet.Model.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -12,40 +14,69 @@ namespace RestWithAspNet.Services.Implementations
 
         public PersonServiceImplementation(MySqlContext context) 
         { 
-            _Context= context;
+            _Context = context;
         }
 
-        public Person Create(Person person)
-        {
-            _Context.Add(person);
-            _Context.SaveChanges();
-
-            return person;
-        }
-
-        public void Delete(long id)
-        {
-            var person = _Context.Persons.FirstOrDefault(x => x.Id == id);
-            _Context.Remove(person);
-            _Context.SaveChanges();
-        }
-
+        
         public List<Person> FindAll()
         {
              return _Context.Persons.ToList();
         }
-
-     
-
         public Person FindBayID(long id)
         {
-            return _Context.Persons.FirstOrDefault(x => x.Id == id);
+            return _Context.Persons.FirstOrDefault(p => p.Id.Equals(id));
         }
-
-        public Person Updade(Person person)
+        public Person Create(Person person)
         {
+            try
+            {
+                _Context.Add(person);
+                _Context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
             return person;
         }
-
+        public Person Updade(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+           
+            var result = _Context.Persons.FirstOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _Context.Entry(result).CurrentValues.SetValues(person);
+                    _Context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return person;
+        }
+        public void Delete(long id)
+        {
+            var result = _Context.Persons.FirstOrDefault(p => p.Id.Equals(id));
+            if (result != null) 
+            {
+                try
+                {
+                    _Context.Persons.Remove(result);
+                    _Context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            } 
+        }
+        private bool Exists(long id)
+        {
+            return _Context.Persons.Any(p=> p.Id.Equals(id));
+        }
     }
 }
