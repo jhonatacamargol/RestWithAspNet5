b681a0e990cb39eb40cfe2ab29b073154324be62
jhonatacamargol <jhonatapires32@hotmail.com>
2023-01-10 19:20:41 -0300

    atualização migration

---
+++ 02_RestWithAspNet_aplication/RestWithAspNet/RestWithAspNet/db/dataset/V2__Populatete_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (id, adress, first_name, gender, last_name) VALUES 
+(1,'Curitiba','Jair','home','Camargo'),
+(2,'São Paulo','Airton','homem','Senna'),
+(3,'Curitiba','Jhonata','home','Camargo'),
+(4,'Curitiba','Juliano','home','Silva'),
+(5,'Cadeia','Lula','home','Silva'),
+(6,'Africa do Sul','Neuson','homem','Ainda'),
+(7,'Paraguai', 'joana', 'Home', 'da Silva');
---
+++ 02_RestWithAspNet_aplication/RestWithAspNet/RestWithAspNet/db/migrations/V1__Create_Table_Person.sql
@@ -0,0 +1,8 @@
+create table if not exists person (
+   id bigint(20) not null auto_increment,
+   first_name varchar(80) not null,
+   last_name varchar(80) not null,
+   address varchar(100) not null,
+   gender varchar(6) not null,
+  primary key (id)
+);
